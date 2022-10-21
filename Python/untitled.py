d6ee461f5d86bf1d59bffafaaf7b1d20f826d944
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2022-10-21 05:20:53 +0300

    Update untitled.py

--- Python/untitled.py
+++ Python/untitled.py
@@ -1,33 +1,74 @@
-number = int(input('введите число '))
-sum = 0 
-while number > 0:
-    sum = sum + number % 10
-    number = number // 10
-print("Сумма цифр:", sum)
+length = int(input('введите длину списка '))
+new_list = []
+sum = 0
+for _ in range(length):
+    num=int(input())
+    new_list.append(num)
+for i in range(1, length):
+    if i %2>0:
+        sum+= new_list[i]
+print(sum)
 
-n = int(input('введите число N '))
-count =1 
-for i in range(1, n+1):
-    count = count*i
-    print(count, end=' ')
+number = int(input('введите длину списка '))
+first_list = []
+second_list=[]
+sum = 0
+for _ in range(number):
+    n=int(input())
+    first_list.append(n)
+for i in range((number-1)//2+1):
+    m=first_list[i] * first_list[number-i-1]
+    second_list.append(m)
+print(second_list)
 
-num = int(input('введите число N '))
-c=0
-for i in range(1, num+1):
-    c=c+(1+1/i)**i
-print(c)
+import math
+number_float = int(input('введите длину списка '))
+list_float = []
+for _ in range(number_float):
+    n=float(input())
+    if n > 0:
+        n=n-math.trunc(n)
+    list_float.append(n)
+print(list_float)
+minn=float(list_float[0])
+maxx=float(list_float[0])
+for i in range(0, number_float):
+    if list_float[i]<minn:
+        minn=list_float[i]
+    elif list_float[i]>maxx:
+        maxx=list_float[i]
+print(maxx-minn)
 
-from random import randint
-nlist=int(input('введите число '))
-list=[]
-for i in range(nlist):
-    list.append(randint(-nlist, nlist))
-file=1
-with open('file.txt', 'w') as txt:
-    for i in range(nlist):
-        txt.write(str(randint(0, nlist-1))+ '\n')
-with open('file.txt', 'r') as txt:
-    txt=txt.read().splitlines()
-    for number in txt:
-        file*=list[int(number)]
-print(file)
+
+digit_d = int(input('введите число '))
+digit_b=''
+while digit_d>0:
+    digit_b=digit_b+str(digit_d%2)
+    digit_d=digit_d//2
+print(digit_b)
+
+f = int(input("введите число Фирбоначчи "))
+fp1 = fp2 = 1
+fibn=[]
+fibp=[]
+fibf=[]
+fn1=1
+fn2=-1
+for i in range(-f, -2):
+    fib_n = fn1 - fn2
+    fn1 = fn2
+    fn2 = fib_n
+    fibn.append(fn2)
+fibn.reverse()
+fibn.append(-1)
+fibn.append(1)
+fibn.append(0)
+fibn.append(1)
+fibn.append(1)
+for i in range(2, f):
+    fib_p = fp1 + fp2
+    fp1 = fp2
+    fp2 = fib_p
+    fibp.append(fp2)
+fibf=fibn+fibp
+print(fibf)
