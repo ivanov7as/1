b8e45d52ace0cb19b6efe7cc4648a50a124cf9f4
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2022-10-25 00:39:25 +0300

    Update untitled.py

--- Python/untitled.py
+++ Python/untitled.py
@@ -1,74 +1,29 @@
-length = int(input('введите длину списка '))
-new_list = []
-sum = 0
-for _ in range(length):
-    num=int(input())
-    new_list.append(num)
-for i in range(1, length):
-    if i %2>0:
-        sum+= new_list[i]
-print(sum)
+number = float(input('введите число '))
+d = float(input('введите точность '))
+s = str(d)
+numoverdot = (abs(s.find('.') - len(s)) - 1)
+print(round(number, numoverdot))
 
-number = int(input('введите длину списка '))
-first_list = []
-second_list=[]
-sum = 0
-for _ in range(number):
-    n=int(input())
-    first_list.append(n)
-for i in range((number-1)//2+1):
-    m=first_list[i] * first_list[number-i-1]
-    second_list.append(m)
-print(second_list)
+n=int(input('введите число N '))
+for i in range(2, n//2 + 1):
+    if (n % i == 0):
+        print(i)
+        n //= i
 
-import math
-number_float = int(input('введите длину списка '))
-list_float = []
-for _ in range(number_float):
-    n=float(input())
-    if n > 0:
-        n=n-math.trunc(n)
-    list_float.append(n)
-print(list_float)
-minn=float(list_float[0])
-maxx=float(list_float[0])
-for i in range(0, number_float):
-    if list_float[i]<minn:
-        minn=list_float[i]
-    elif list_float[i]>maxx:
-        maxx=list_float[i]
-print(maxx-minn)
+first = [1, 2 ,2 ,3 ,4, 4, 4 ,5, 5 ,6 ,7 ,8, 8, 8]
+second = []
+for i in range(1, len(first)):
+    if first.count(i)==1:
+        second.append(i)
+print(second)
 
-
-digit_d = int(input('введите число '))
-digit_b=''
-while digit_d>0:
-    digit_b=digit_b+str(digit_d%2)
-    digit_d=digit_d//2
-print(digit_b)
-
-f = int(input("введите число Фирбоначчи "))
-fp1 = fp2 = 1
-fibn=[]
-fibp=[]
-fibf=[]
-fn1=1
-fn2=-1
-for i in range(-f, -2):
-    fib_n = fn1 - fn2
-    fn1 = fn2
-    fn2 = fib_n
-    fibn.append(fn2)
-fibn.reverse()
-fibn.append(-1)
-fibn.append(1)
-fibn.append(0)
-fibn.append(1)
-fibn.append(1)
-for i in range(2, f):
-    fib_p = fp1 + fp2
-    fp1 = fp2
-    fp2 = fib_p
-    fibp.append(fp2)
-fibf=fibn+fibp
-print(fibf)
+from random import randint
+k = int(input('введите  степень k '))
+num=[randint(0,100) for i in range(k)]+[randint(1,100)]
+eq=' + '.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(num) if j][::-1])
+eq=eq.replace('x^1', 'x')
+eq=eq.replace('x^0', '')
+eq=eq+' = 0'
+print(eq)
+with open('file.txt', 'w') as file:
+    file.write(eq)
