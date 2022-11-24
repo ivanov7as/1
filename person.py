1756b076f2a187c786b78354de917371947b1ef6
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2022-11-24 21:03:37 +0300

    Update person.py

--- person.py
+++ person.py
@@ -1,12 +1,23 @@
+import csv
+def id():
+    with open('data.csv', 'a', encoding='utf-8') as file:
+        file.write("ID "+input('введите ID ')+'\n')
 def surname():
-    with open('data.txt', 'a', encoding='utf-8') as file:
+    with open('data.csv', 'a', encoding='utf-8') as file:
         file.write("Фамилия "+input('введите фамилию ')+'\n')
 def name():
-    with open('data.txt', 'a', encoding='utf-8') as file:
+    with open('data.csv', 'a', encoding='utf-8') as file:
         file.write("Имя "+input('введите имя ')+'\n')
 def number():
-    with open('data.txt', 'a', encoding='utf-8') as file:
+    with open('data.csv', 'a', encoding='utf-8') as file:
         file.write("Номер "+input('введите номер ')+'\n')
+def age():
+    with open('data.csv', 'a', encoding='utf-8') as file:
+        file.write("Возраст "+input('введите возраст ')+'\n')
+def work():
+    with open('data.csv', 'a', encoding='utf-8') as file:
+        file.write("Должность "+input('введите должность ')+'\n')
 def null():
-    with open('data.txt', 'a') as file:
-        file.write("****************"+'\n')
+    with open('data.csv', 'a', encoding='utf-8') as file:
+        file.write(""+'\n')
+
